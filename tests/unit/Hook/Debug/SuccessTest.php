38e661dfb5132b6f7c1b222158efc138c51511cc
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2025-07-24 13:19:28 +0200

    Add 'no tag yet' test case

--- tests/unit/Hook/Debug/SuccessTest.php
+++ tests/unit/Hook/Debug/SuccessTest.php
@@ -18,6 +18,7 @@ use CaptainHook\App\Hook\Debug;
 use CaptainHook\App\Mockery as CHMockery;
 use Exception;
 use PHPUnit\Framework\TestCase;
+use SebastianFeldmann\Git\Operator\Info;
 
 class SuccessTest extends TestCase
 {
@@ -37,6 +38,28 @@ class SuccessTest extends TestCase
         $repository   = $this->createRepositoryMock();
         $action       = new Action('\\' . Debug::class);
         $infoOperator = $this->createGitInfoOperator('1.0.0');
+
+        $io->expects($this->once())->method('getArguments')->willReturn(['foo' => 'bar']);
+        $io->expects($this->atLeast(3))->method('write');
+        $repository->expects($this->exactly(1))->method('getInfoOperator')->willReturn($infoOperator);
+
+        $debug = new Success();
+        $debug->execute($config, $io, $repository, $action);
+
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Tests Debug::execute
+     */
+    public function testExecuteWithoutTags(): void
+    {
+        $config       = $this->createConfigMock(true);
+        $io           = $this->createIOMock();
+        $repository   = $this->createRepositoryMock();
+        $action       = new Action('\\' . Debug::class);
+        $infoOperator = $this->getMockBuilder(Info::class)->disableOriginalConstructor()->getMock();
+        $infoOperator->method('getCurrentTag')->willThrowException(new Exception('foo'));
 
         $io->expects($this->once())->method('getArguments')->willReturn(['foo' => 'bar']);
         $io->expects($this->atLeast(3))->method('write');
