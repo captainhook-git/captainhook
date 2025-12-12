f4485d2a5db16a37053ffe7916f0808f619b430d
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2025-12-12 11:35:01 +0100

    Prepare version 5.27.4

--- src/CH.php
+++ src/CH.php
@@ -26,12 +26,12 @@ final class CH
     /**
      * Current CaptainHook version
      */
-    public const VERSION = '5.27.3';
+    public const VERSION = '5.27.4';
 
     /**
      * Release date of the current version
      */
-    public const RELEASE_DATE = '2025-11-22';
+    public const RELEASE_DATE = '2025-12-12';
 
     /**
      * Default configuration file
